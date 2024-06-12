f5706b15ec81f2f596aeed77ff93dce5a730f1f7
SineMunchs <167076042+SineMunchs@users.noreply.github.com>
2024-06-12 17:32:20 +0200

    commit stroke

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,12 +7,14 @@
       <div class="p-1">
         <img src="../../public/images/never4.svg" alt="HomeView.vue" class="-mr-2 mb-4">
       </div>
+
+
       <div class="video-container">
-        <video class="h-40" autoplay loop muted>
-          <source src="../../video/dans.mp4" type="video/mp4">
+        <video class="mb-20" autoplay loop muted>
+          <source src="../../video/dans1.mp4" type="video/mp4">
         </video>
-        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 271 361" class="animated-svg">
-          <path class="cls-1" d="m60.1,25.5C-13.9,161.5,267.67,211.91,220.1,345.5"/>
+        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 271 361" class="animated-svg ">
+          <path class="cls-1 path" d="m60.1,25.5C-13.9,161.5,267.67,211.91,220.1,345.5"/>
         </svg>
       </div>
     </div>
@@ -20,41 +22,41 @@
 </template>
 
 
-
 <style scoped>
-
-.cls-1{
-  
-  fill: none;
-  stroke: #000000;
-  stroke-width: 0.3rem;
-  stroke-miterlimit: 10;
-  transition: stroke-dashoffset 3s ease;
-}
-.video-container:hover .cls{
-  stroke-dashoffset: 0;
-}
-.cls-1 {
-  stroke-dasharray: 450;
-  stroke-dashoffset: 450;
-}
 .video-container {
   position: relative;
 }
 
+.video-container video {
+  width: 40%;
+  height: auto;
+}
+
+.video-container svg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 40%;
+  height: 100%;
+}
 .animated-svg {
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  animation: animateSvg 5s infinite;
 }
 
+.path {
+  fill: none;
+  stroke: rgb(31, 45, 233);
+  stroke-width: 10;
+  stroke-dasharray: 400;
+  stroke-dashoffset: 400;
+  transition: stroke-dashoffset 1s ease;
+}
 
-@keyframes animateSvg {
-  0% { transform: translateY(0); }
-  50% { transform: translateY(10px); }
-  100% { transform: translateY(0); }
+.video-container:hover .path {
+  stroke-dashoffset: 0;
 }
 </style>
